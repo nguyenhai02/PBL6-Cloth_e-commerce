331e8f72fe7dc69439b5521b47d793ca4cd5ac84
ThanhHien1 <nha755261@gmail.com>
2023-09-20 11:57:12 +0700

    Add aplication

---
+++ App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Thanh Hien on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/App/Views/Register/RegisterView.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterView.swift
+//  App
+//
+//  Created by Thanh Hien on 19/09/2023.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
